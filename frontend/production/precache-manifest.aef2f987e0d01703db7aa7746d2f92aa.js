3f0b52ee0a315e313ca21fed98a41a9e72aa0425
Sreang Rathanak <sreang.rathanak@framgia.com>
2020-04-01 21:45:21 +0700

    Production UUI

---
+++ frontend/production/precache-manifest.aef2f987e0d01703db7aa7746d2f92aa.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd52e923d9e4030edfc93ceba70b8aa2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54ffbdb191b7650ad9bd",
+    "url": "/static/css/2.6ea3de80.chunk.css"
+  },
+  {
+    "revision": "ba330025b5a0d0d4830d",
+    "url": "/static/css/main.ec3908e8.chunk.css"
+  },
+  {
+    "revision": "54ffbdb191b7650ad9bd",
+    "url": "/static/js/2.ea852b8c.chunk.js"
+  },
+  {
+    "revision": "73da30d41f9ad1e9ca43b2b589509622",
+    "url": "/static/js/2.ea852b8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba330025b5a0d0d4830d",
+    "url": "/static/js/main.af06e94a.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  },
+  {
+    "revision": "bd01e22ad82e19ebda62d7fee303802a",
+    "url": "/static/media/full_logo.bd01e22a.svg"
+  },
+  {
+    "revision": "d3ddd6025a06a78535b0d432d14905bf",
+    "url": "/static/media/gb_flag.d3ddd602.svg"
+  },
+  {
+    "revision": "5a13865d2bcaa01d31483c08c8903ea7",
+    "url": "/static/media/kh_flag.5a13865d.svg"
+  },
+  {
+    "revision": "0aba2b333ab0d292b57dcf5611a0f776",
+    "url": "/static/media/oval.0aba2b33.svg"
+  }
+]);
